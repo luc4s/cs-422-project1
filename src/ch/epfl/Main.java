4ccdedf7aa1bb3459ba8a03111e58efe4834bca0
Lucas <luca.monnin@gmail.com>
2018-03-19 13:05:29 +0100

    Started writing benchmarks

--- src/ch/epfl/Main.java
+++ src/ch/epfl/Main.java
@@ -11,37 +11,135 @@ import ch.epfl.dias.store.row.RowStore;
 
 public class Main {
 
+    public static DataType[] lineitemSchema = new DataType[]{
+            DataType.INT,
+            DataType.INT,
+            DataType.INT,
+            DataType.INT,
+            DataType.INT,
+            DataType.DOUBLE,
+            DataType.DOUBLE,
+            DataType.DOUBLE,
+            DataType.STRING,
+            DataType.STRING,
+            DataType.STRING,
+            DataType.STRING,
+            DataType.STRING,
+            DataType.STRING,
+            DataType.STRING,
+            DataType.STRING};
+
+	public static DataType[] orderSchema = new DataType[]{
+	                DataType.INT,
+	                DataType.INT,
+	                DataType.STRING,
+	                DataType.DOUBLE,
+	                DataType.STRING,
+	                DataType.STRING,
+	                DataType.STRING,
+	                DataType.INT,
+	                DataType.STRING};
+	
+	public static DataType[] schema = new DataType[]{ 
+			DataType.INT, 
+			DataType.INT, 
+			DataType.INT, 
+			DataType.INT, 
+			DataType.INT,
+			DataType.INT, 
+			DataType.INT, 
+			DataType.INT, 
+			DataType.INT, 
+			DataType.INT };
+
 	public static void main(String[] args) {
 
-		DataType[] schema = new DataType[] { DataType.INT, DataType.INT, DataType.INT, DataType.INT, DataType.INT,
-				DataType.INT, DataType.INT, DataType.INT, DataType.INT, DataType.INT };
 
-		DataType[] orderSchema = new DataType[] { DataType.INT, DataType.INT, DataType.STRING, DataType.DOUBLE,
-				DataType.STRING, DataType.STRING, DataType.STRING, DataType.INT, DataType.STRING };
-
-		schema = new DataType[] { DataType.INT, DataType.INT, DataType.INT, DataType.INT, DataType.INT, DataType.INT,
-				DataType.INT, DataType.INT, DataType.INT, DataType.INT };
-
-		 RowStore rowstore = new RowStore(orderSchema, "input/orders_small.csv", "\\|");
+		 RowStore rowstore = new RowStore(orderSchema, "input/orders_big.csv", "\\|");
 		 rowstore.load();
 
-		 PAXStore paxstore = new PAXStore(orderSchema, "input/orders_small.csv", "\\|", 3);
-		 paxstore.load();
-		 ch.epfl.dias.ops.volcano.Scan scan = new ch.epfl.dias.ops.volcano.Scan(paxstore);
-		 DBTuple currentTuple = scan.next();
-		 while (!currentTuple.eof) {
-		 	System.out.println(currentTuple.getFieldAsInt(1));
-		 	currentTuple = scan.next();
-		 }
-
+		 
 		 ColumnStore columnstoreData = new ColumnStore(schema, "input/data.csv", ",");
 		 columnstoreData.load();
+		 
+		 testPAXVolcano();
+
 		
 		 ch.epfl.dias.ops.block.Scan blockScan = new ch.epfl.dias.ops.block.Scan(columnstoreData);
 		 ch.epfl.dias.ops.block.Select sel = new ch.epfl.dias.ops.block.Select(blockScan, BinaryOp.EQ, 3, 6);
 		 ch.epfl.dias.ops.block.ProjectAggregate agg = new ch.epfl.dias.ops.block.ProjectAggregate(sel, Aggregate.COUNT, DataType.INT, 2);
 		 DBColumn[] result = agg.execute();
 		 int output = result[0].getAsInteger()[0];
-		 System.out.println(output);
+	}
+	
+	public static void testPAXVolcano() {
+		 PAXStore orders = new PAXStore(orderSchema, "input/orders_big.csv", "\\|", 10);
+		 PAXStore lines = new PAXStore(lineitemSchema, "input/lineitem_big.csv", "\\|", 10);
+		 System.out.println("[PAX] Loading datasets...");
+		 lines.load();
+		 orders.load();
+		 System.out.println("[PAX] Datasets loaded.");
+
+		 /* QUERY 1 */
+		 ch.epfl.dias.ops.volcano.Scan scanLines = new ch.epfl.dias.ops.volcano.Scan(lines);
+		 ch.epfl.dias.ops.volcano.Select select = new ch.epfl.dias.ops.volcano.Select(scanLines, BinaryOp.LT, 4, 50);
+		 ch.epfl.dias.ops.volcano.Project project = new ch.epfl.dias.ops.volcano.Project(select, new int[] { 0, 1, 2, 14, 15 } );
+		 
+		 System.out.println("SELECT L_ORDERKEY, L_PARTKEY, L_SUPPKEY, L_SHIPMODE, L_COMMENT FROM LINEITEM WHERE L_QUANTITY < 50");
+		 long time = System.nanoTime();
+		 project.open();
+		 DBTuple tuple;
+		 while (!(tuple = project.next()).eof) {}
+		 System.out.println("\t executed in " + parseTime(System.nanoTime() - time));
+		 
+		 /* QUERY 2 */
+		 scanLines = new ch.epfl.dias.ops.volcano.Scan(lines);
+		 ch.epfl.dias.ops.volcano.Scan scanOrders = new ch.epfl.dias.ops.volcano.Scan(lines);
+		 ch.epfl.dias.ops.volcano.HashJoin join = new ch.epfl.dias.ops.volcano.HashJoin(scanLines, scanOrders, 0, 0);
+		 project = new ch.epfl.dias.ops.volcano.Project(join, new int[] { 4, 19 } );
+
+		 time = System.nanoTime();
+		 System.out.println("SELECT L.L_QUANTITY, O.O_TOTALPRICE FROM LINEITEM L, ORDERS O WHERE L.ORDERKEY = O.ORDERKEY");
+		 project.open();
+		 while (!(tuple = project.next()).eof) {}
+		 System.out.println("\t executed in " + parseTime(System.nanoTime() - time));
+		 
+		 /*  QUERY 3 */
+		 scanLines = new ch.epfl.dias.ops.volcano.Scan(lines);
+		 select = new ch.epfl.dias.ops.volcano.Select(scanLines, BinaryOp.LT, 4, 50);
+		 ch.epfl.dias.ops.volcano.ProjectAggregate agg = new ch.epfl.dias.ops.volcano.ProjectAggregate(select, Aggregate.AVG, DataType.DOUBLE, 7);
+
+		 time = System.nanoTime();
+		 System.out.println("SELECT AVG(L_TAX) FROM LINEITEM WHERE L_QUANTITY < 50");
+		 agg.open();
+		 while (!(tuple = agg.next()).eof) {}
+		 System.out.println("\t executed in " + parseTime(System.nanoTime() - time));
+
+		 /* QUERY 4 */
+		 scanLines = new ch.epfl.dias.ops.volcano.Scan(lines);
+		 scanOrders = new ch.epfl.dias.ops.volcano.Scan(lines);
+		 join = new ch.epfl.dias.ops.volcano.HashJoin(scanLines, scanOrders, 0, 0);
+		 agg = new ch.epfl.dias.ops.volcano.ProjectAggregate(join, Aggregate.MAX, DataType.DOUBLE, 19);
+
+		 time = System.nanoTime();
+		 System.out.println("SELECT MAX(O.O_TOTALPRICE) FROM LINEITEM L, ORDERS O WHERE L.ORDERKEY = O.ORDERKEY");
+		 agg.open();
+		 while (!(tuple = agg.next()).eof) {}
+		 System.out.println("\t executed in " + parseTime(System.nanoTime() - time));
+		 
+	}
+	
+	private static String parseTime(long time) {
+		String[] units = new String[] { "ns", "ms", "s", "min", "h" };
+		double[] scale  = new double[] { 1000000.0, 1000.0, 60.0, 60.0 };
+		
+		double t = time;
+		for (int i = 0; i < scale.length; i++) {
+			if (t < scale[i])
+				return Double.toString(t) + units[i];
+			else 
+				t /= scale[i];
+		}
+		return Double.toString(t) + "h";
 	}
 }
